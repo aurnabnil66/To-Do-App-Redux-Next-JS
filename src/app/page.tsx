6978f01c75ae3d73f4c28b626c5584c89e1b2fc9
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2024-12-28 03:10:41 +0600

    project initialized and redux added and action types created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,125 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+      <div className="fixed top-0 left-0 text-center w-full header bg-violet-600 py-4 text-white font-bold text-lg shadow-lg">
+        Simple Todo Application with Redux & Next JS
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+        <div>
+          <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+            <img src="./images/notes.png" className="w-6 h-6" alt="Add todo" />
+            <input
+              type="text"
+              placeholder="Type your todo"
+              className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              type="submit"
+              className="appearance-none w-8 h-8 bg-[url('./images/plus.png')] bg-no-repeat bg-contain"
+            ></button>
+          </form>
+
+          <ul className="flex justify-between my-4 text-xs text-gray-500">
+            <li className="flex space-x-1 cursor-pointer">
+              <img
+                className="w-4 h-4"
+                src="./images/double-tick.png"
+                alt="Complete"
+              />
+              <span>Complete All Tasks</span>
+            </li>
+            <li className="cursor-pointer">Clear completed</li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <hr className="mt-4" />
+
+        <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+          <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
+            <div className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
+              <input
+                type="checkbox"
+                className="opacity-0 absolute rounded-full"
+              />
+              <svg
+                className="hidden fill-current w-3 h-3 text-green-500 pointer-events-none"
+                viewBox="0 0 20 20"
+              >
+                <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+              </svg>
+            </div>
+
+            <div className="select-none flex-1 line-through">
+              Learn React from Learn with Sumit YouTube Channel
+            </div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 bg-green-500"></div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500"></div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
+
+            <img
+              src="./images/cancel.png"
+              className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
+              alt="Cancel"
+            />
+          </div>
+
+          <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
+            <div className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
+              <input
+                type="checkbox"
+                className="opacity-0 absolute rounded-full"
+              />
+              <svg
+                className="hidden fill-current w-3 h-3 text-green-500 pointer-events-none"
+                viewBox="0 0 20 20"
+              >
+                <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+              </svg>
+            </div>
+
+            <div className="select-none flex-1">
+              Learn Redux from Think in a Redux way course
+            </div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 bg-green-500"></div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500"></div>
+
+            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
+
+            <img
+              src="./images/cancel.png"
+              className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
+              alt="Cancel"
+            />
+          </div>
+        </div>
+
+        <hr className="mt-4" />
+
+        <div className="mt-4 flex justify-between text-xs text-gray-500">
+          <p>2 tasks left</p>
+          <ul className="flex space-x-1 items-center text-xs">
+            <li className="cursor-pointer font-bold">All</li>
+            <li>|</li>
+            <li className="cursor-pointer">Incomplete</li>
+            <li>|</li>
+            <li className="cursor-pointer">Complete</li>
+            <li></li>
+            <li></li>
+            <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
+            <li className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
+            <li className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
